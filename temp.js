a0567e3d802a5da5b7a4e15570a0da5a75a4cbb2
Sheharyar Rao <k.sheharyarshahid@gmail.com>
2025-06-11 15:13:31 +0500

    Initial Commit

---
+++ temp.js
@@ -0,0 +1,107 @@
+// Simple useless JavaScript file - 150 lines
+
+let nothing = 0;
+
+function doNothing1() {}
+function doNothing2() {}
+function doNothing3() {}
+function doNothing4() {}
+function doNothing5() {}
+function doNothing6() {}
+function doNothing7() {}
+function doNothing8() {}
+function doNothing9() {}
+function doNothing10() {}
+
+for (let i = 0; i < 10; i++) {
+    console.log("Line", i);
+}
+
+let emptyArray = [];
+
+for (let i = 0; i < 20; i++) {
+    emptyArray.push(i);
+}
+
+let uselessObject = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5
+};
+
+function emptyLoop() {
+    for (let i = 0; i < 5; i++) {}
+}
+
+function dummyA() {}
+function dummyB() {}
+function dummyC() {}
+function dummyD() {}
+function dummyE() {}
+
+let x = 1;
+let y = 2;
+let z = x + y;
+
+x++;
+y--;
+z = x * y;
+
+(() => {
+    let foo = "bar";
+    foo += "baz";
+})();
+
+// Padding
+function pad1() {}
+function pad2() {}
+function pad3() {}
+function pad4() {}
+function pad5() {}
+function pad6() {}
+function pad7() {}
+function pad8() {}
+function pad9() {}
+function pad10() {}
+function pad11() {}
+function pad12() {}
+function pad13() {}
+function pad14() {}
+function pad15() {}
+function pad16() {}
+function pad17() {}
+function pad18() {}
+function pad19() {}
+function pad20() {}
+function pad21() {}
+function pad22() {}
+function pad23() {}
+function pad24() {}
+function pad25() {}
+function pad26() {}
+function pad27() {}
+function pad28() {}
+function pad29() {}
+function pad30() {}
+function pad31() {}
+function pad32() {}
+function pad33() {}
+function pad34() {}
+function pad35() {}
+function pad36() {}
+function pad37() {}
+function pad38() {}
+function pad39() {}
+function pad40() {}
+function pad41() {}
+function pad42() {}
+function pad43() {}
+function pad44() {}
+function pad45() {}
+function pad46() {}
+function pad47() {}
+function pad48() {}
+function pad49() {}
+function pad50() {}
